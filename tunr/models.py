35c135fe87712cde3d68d4b93afb7f07e5913a25
Leonard Moses <qalexeon@polaris.myfiosgateway.com>
2022-03-01 17:07:42 -0500

    created models and superuser

--- tunr/models.py
+++ tunr/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Artist(models.Model):
+    name = models.CharField(max_length=100)
+    nationality = models.CharField(max_length=100)
+    photo_url = models.TextField()
+
+    def __str__(self):
+        return self.name
+    
+
+class Song(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='songs')
